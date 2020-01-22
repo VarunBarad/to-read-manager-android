cac0257d74cc48528b522cca2adb9c63eb21e75d
Varun Barad <varun@varunbarad.com>
2020-01-22 22:57:37 +0530

    Enable data-binding.

--- app/src/main/java/com/varunbarad/toreadmanager/MainActivity.kt
+++ app/src/main/java/com/varunbarad/toreadmanager/MainActivity.kt
@@ -2,11 +2,14 @@ package com.varunbarad.toreadmanager
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.varunbarad.toreadmanager.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewBinding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        this.viewBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
