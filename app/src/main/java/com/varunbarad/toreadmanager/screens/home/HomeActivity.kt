063ae4b03917db4948b9949838cc83fcc290aee8
Varun Barad <varun@varunbarad.com>
2020-01-24 00:18:24 +0530

    Show each entry on home screen in a list

--- app/src/main/java/com/varunbarad/toreadmanager/screens/home/HomeActivity.kt
+++ app/src/main/java/com/varunbarad/toreadmanager/screens/home/HomeActivity.kt
@@ -1,12 +1,17 @@
 package com.varunbarad.toreadmanager.screens.home
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.varunbarad.toreadmanager.R
 import com.varunbarad.toreadmanager.databinding.ActivityHomeBinding
+import com.varunbarad.toreadmanager.screens.home.entries_adapter.EntriesListAdapter
 import com.varunbarad.toreadmanager.util.Dependencies
 import com.varunbarad.toreadmanager.util.ThreadSchedulers
+import com.varunbarad.toreadmanager.util.toUiToReadEntry
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.subscribeBy
 
@@ -15,12 +20,28 @@ class HomeActivity : AppCompatActivity() {
 
     private lateinit var viewBinding: ActivityHomeBinding
 
+    private val toReadEntriesListAdapter: EntriesListAdapter = EntriesListAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         this.viewBinding = DataBindingUtil.setContentView(
             this,
             R.layout.activity_home
         )
+
+        val recyclerViewLayoutManager = LinearLayoutManager(
+            this.viewBinding.recyclerViewToReadEntries.context,
+            LinearLayoutManager.VERTICAL,
+            false
+        )
+        this.viewBinding.recyclerViewToReadEntries.layoutManager = recyclerViewLayoutManager
+        this.viewBinding.recyclerViewToReadEntries.addItemDecoration(
+            DividerItemDecoration(
+                this.viewBinding.recyclerViewToReadEntries.context,
+                recyclerViewLayoutManager.orientation
+            )
+        )
+        this.viewBinding.recyclerViewToReadEntries.adapter = this.toReadEntriesListAdapter
     }
 
     override fun onStart() {
@@ -30,19 +51,50 @@ class HomeActivity : AppCompatActivity() {
             Dependencies.getToReadDatabase(this)
                 .linksDao()
                 .getEntries()
+                .map { entries -> entries.map { it.toUiToReadEntry() } }
                 .subscribeOn(ThreadSchedulers.io())
                 .observeOn(ThreadSchedulers.main())
                 .subscribeBy(
                     onNext = { entries ->
-                        val message: String =
-                            entries.joinToString(
-                                separator = "\n",
-                                transform = { "${it.title} -> ${it.url}" }
-                            )
-
-                        this.viewBinding.tempTextView.text = message
+                        this.toReadEntriesListAdapter.submitList(entries)
                     }
                 )
+        )
+
+        this.serviceDisposables.add(
+            this.toReadEntriesListAdapter
+                .getOpenToReadEntryObservable()
+                .subscribeBy(onNext = { entry ->
+                    Toast.makeText(
+                        this,
+                        "Open -> ${entry.title}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                })
+        )
+
+        this.serviceDisposables.add(
+            this.toReadEntriesListAdapter
+                .getArchiveToReadEntryObservable()
+                .subscribeBy(onNext = { entry ->
+                    Toast.makeText(
+                        this,
+                        "Archive -> ${entry.title}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                })
+        )
+
+        this.serviceDisposables.add(
+            this.toReadEntriesListAdapter
+                .getDeleteToReadEntryObservable()
+                .subscribeBy(onNext = { entry ->
+                    Toast.makeText(
+                        this,
+                        "Delete -> ${entry.title}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                })
         )
     }
 
