f2c21797f7d27d6ec44c21b07014fa6dc05b0dbe
Varun Barad <varun@varunbarad.com>
2020-01-23 20:03:58 +0530

    Add room-model for links table.

---
+++ app/src/main/java/com/varunbarad/toreadmanager/local_database/models/DbLink.kt
@@ -0,0 +1,19 @@
+package com.varunbarad.toreadmanager.local_database.models
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.Index
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "Links",
+    indices = [
+        Index(value = ["url"], unique = true)
+    ]
+)
+data class DbLink(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int? = null,
+    @ColumnInfo(name = "url") val url: String,
+    @ColumnInfo(name = "title") val title: String
+)
