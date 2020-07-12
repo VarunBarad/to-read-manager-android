e1704a3e920ec538e1223397037e59f7004184ca
Varun Barad <varun@varunbarad.com>
2020-07-12 20:02:58 +0530

    Inject `LinksDao` in archived-entries screen using Dagger

--- app/src/main/java/com/varunbarad/toreadmanager/di/AppComponent.kt
+++ app/src/main/java/com/varunbarad/toreadmanager/di/AppComponent.kt
@@ -1,6 +1,7 @@
 package com.varunbarad.toreadmanager.di
 
 import com.varunbarad.toreadmanager.ToReadManagerApplication
+import com.varunbarad.toreadmanager.screens.home.fragments.archived.EntriesArchivedFragment
 import com.varunbarad.toreadmanager.screens.home.fragments.current.EntriesCurrentFragment
 import dagger.Component
 import javax.inject.Scope
@@ -14,6 +15,7 @@ import javax.inject.Scope
 interface AppComponent {
     fun inject(target: ToReadManagerApplication)
     fun inject(target: EntriesCurrentFragment)
+    fun inject(target: EntriesArchivedFragment)
 }
 
 @Scope
--- app/src/main/java/com/varunbarad/toreadmanager/screens/home/fragments/archived/EntriesArchivedFragment.kt
+++ app/src/main/java/com/varunbarad/toreadmanager/screens/home/fragments/archived/EntriesArchivedFragment.kt
@@ -8,11 +8,16 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.snackbar.Snackbar
+import com.varunbarad.toreadmanager.ToReadManagerApplication
 import com.varunbarad.toreadmanager.databinding.FragmentEntriesArchivedBinding
-import com.varunbarad.toreadmanager.local_database.LinksDatabase
-import com.varunbarad.toreadmanager.util.*
+import com.varunbarad.toreadmanager.local_database.LinksDao
+import com.varunbarad.toreadmanager.util.ThreadSchedulers
+import com.varunbarad.toreadmanager.util.openLinkInBrowser
+import com.varunbarad.toreadmanager.util.toDbLink
+import com.varunbarad.toreadmanager.util.toUiToReadEntry
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.subscribeBy
+import javax.inject.Inject
 
 class EntriesArchivedFragment : Fragment() {
     companion object {
@@ -20,14 +25,20 @@ class EntriesArchivedFragment : Fragment() {
     }
 
     private val serviceDisposables = CompositeDisposable()
-    private val toReadDatabase: LinksDatabase by lazy {
-        Dependencies.getToReadDatabase(this.requireContext())
-    }
+
+    @Inject
+    lateinit var linksDao: LinksDao
 
     private lateinit var viewBinding: FragmentEntriesArchivedBinding
 
     private val toReadEntriesListAdapter: ArchivedEntriesListAdapter =
         ArchivedEntriesListAdapter()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        ToReadManagerApplication.appComponent.inject(this)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -56,8 +67,7 @@ class EntriesArchivedFragment : Fragment() {
         super.onStart()
 
         this.serviceDisposables.add(
-            this.toReadDatabase
-                .linksDao()
+            this.linksDao
                 .getArchivedEntries()
                 .map { entries -> entries.map { it.toUiToReadEntry() } }
                 .subscribeOn(ThreadSchedulers.io())
@@ -82,8 +92,7 @@ class EntriesArchivedFragment : Fragment() {
                 .getUnarchiveToReadEntryObservable()
                 .subscribeBy(onNext = { entry ->
                     this.serviceDisposables.add(
-                        this.toReadDatabase
-                            .linksDao()
+                        this.linksDao
                             .updateEntry(entry.toDbLink().copy(archived = false))
                             .subscribeOn(ThreadSchedulers.io())
                             .observeOn(ThreadSchedulers.main())
@@ -99,8 +108,7 @@ class EntriesArchivedFragment : Fragment() {
                 .getDeleteToReadEntryObservable()
                 .subscribeBy(onNext = { entry ->
                     this.serviceDisposables.add(
-                        this.toReadDatabase
-                            .linksDao()
+                        this.linksDao
                             .deleteEntry(entry.toDbLink())
                             .subscribeOn(ThreadSchedulers.io())
                             .observeOn(ThreadSchedulers.main())
