ae3246c1f4425a558722f3ec8143af8d238e40d1
Varun Barad <varun@varunbarad.com>
2020-07-12 19:49:18 +0530

    Inject dependencies in `Application` class via dagger

--- app/src/main/java/com/varunbarad/toreadmanager/ToReadManagerApplication.kt
+++ app/src/main/java/com/varunbarad/toreadmanager/ToReadManagerApplication.kt
@@ -1,7 +1,25 @@
 package com.varunbarad.toreadmanager
 
 import android.app.Application
+import com.varunbarad.toreadmanager.di.AppComponent
+import com.varunbarad.toreadmanager.di.AppModule
+import com.varunbarad.toreadmanager.di.DaggerAppComponent
 
 class ToReadManagerApplication : Application() {
+    companion object {
+        lateinit var appComponent: AppComponent
+    }
 
+    override fun onCreate() {
+        super.onCreate()
+
+        appComponent = this.buildDaggerGraph()
+        appComponent.inject(this)
+    }
+
+    private fun buildDaggerGraph(): AppComponent {
+        return DaggerAppComponent.builder()
+            .appModule(AppModule(this))
+            .build()
+    }
 }
